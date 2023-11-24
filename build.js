799232e2b51e9e3d7254e16356d38a00d008dac7
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2023-11-24 14:02:14 +0200

    Add files via upload

---
+++ build.js
@@ -0,0 +1,87 @@
+let build = {
+    "Barracks": {
+        "Infantry Attack vs. Player": "PriceMedium",
+        "Infantry Defense vs. Player's Ranged": "PriceLow",
+        "Infantry Defense vs. Player's Cavalry": "PriceLow"
+    },
+    "Range": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+    "Stable": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Workshop": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceMedium"
+    },
+	"Wall": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceMedium"
+    },
+	"Shrine": {
+        "Def": "PriceHigh",
+        "Enhancement2": "PriceHigh",
+        "Enhancement3": "PriceMedium"
+    },
+	"Training yard": {
+        "Enhancement1": "PriceLow",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Great hall": {
+        "Enhancement1": "PriceLow",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Storehouse": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceMedium"
+    },
+	"Smith": {
+        "Enhancement1": "PriceLow",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Rookery": {
+        "Enhancement1": "PriceLow",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Master's tower": {
+        "Enhancement1": "PriceHigh",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceLow"
+    },
+	"Masters tower": {
+        "Enhancement1": "PriceHigh",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceLow"
+    },
+	"Watchtower": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceLow",
+        "Enhancement3": "PriceLow"
+    },
+	"Keep": {
+        "Enhancement1": "PriceHigh",
+        "Enhancement2": "PriceHigh",
+        "Enhancement3": "PriceHigh"
+    },
+	"Dragon pit": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceMedium"
+    },
+	"Dragon": {
+        "Enhancement1": "PriceMedium",
+        "Enhancement2": "PriceMedium",
+        "Enhancement3": "PriceMedium"
+    }
+}
---
+++ json-parse.js
@@ -0,0 +1,182 @@
+function loadBuildings(buildingSelectElement) {
+    buildingSelectElement.innerHTML = '';
+    for (let building in build) {
+        let option = document.createElement('option');
+        option.value = building;
+        option.textContent = building;
+        buildingSelectElement.appendChild(option);
+    }
+
+    var enhancementSelect = buildingSelectElement.parentNode.parentNode.querySelector('.enhancementSelect');
+    loadEnhancements(enhancementSelect);
+}
+
+function loadEnhancements(enhancementSelect) {
+    var buildingSelect = enhancementSelect.parentNode.parentNode.querySelector('.buildingSelect');
+    let selectedBuilding = buildingSelect.value;
+    
+    enhancementSelect.innerHTML = '';
+    let enhancements = build[selectedBuilding];
+    for (let enhancement in enhancements) {
+        let option = document.createElement('option');
+        option.value = enhancements[enhancement];
+        option.textContent = enhancement;
+        enhancementSelect.appendChild(option);
+    }
+}
+
+function addAnotherBuilding() {
+    var newBuildingDiv = document.createElement('div');
+    newBuildingDiv.className = 'buildingBlock';
+
+    newBuildingDiv.innerHTML = `
+        <div class="selectBuilds">
+            <div class="selectBuild">
+                <label for="buildingSelect">Select Building:</label>
+                <select class="buildingSelect"></select>
+            </div>
+            <div class="selectEnhance">
+                <label for="enhancementSelect">Select Enhancement:</label>
+                <select class="enhancementSelect"></select>
+            </div>
+        </div>
+        <div class="selectLevel">
+            <div class="currentLevel">
+                <label for="currentLevel">Current level:</label>
+                <input type="number" class="currentLevel" min="1" max="35" value="1">
+            </div>
+            <div class="targetLevel">
+                <label for="targetLevel">Update level:</label>
+                <input type="number" class="targetLevel" min="1" max="35" value="35">
+            </div>
+        </div>`;
+
+    var container = document.getElementById('buildingBlocksContainer'); // Oletetaan, että tämä on kontti, johon uudet lohkot lisätään
+    container.appendChild(newBuildingDiv);
+
+    var newBuildingSelect = newBuildingDiv.querySelector('.buildingSelect');
+    var newEnhancementSelect = newBuildingDiv.querySelector('.enhancementSelect');
+
+    loadBuildings(newBuildingSelect);
+    newBuildingSelect.addEventListener('change', function() {
+        loadEnhancements(newEnhancementSelect);
+    });
+}
+
+
+
+
+
+function calculateCost() {
+    var aleSlate = parseFloat(document.querySelector('.aleSlate').value) / 100;
+    var aleMarble = parseFloat(document.querySelector('.aleMarble').value) / 100;
+    var aleKeystone = parseFloat(document.querySelector('.aleKeystone').value) / 100;
+    var totalCosts = { slate: 0, marble: 0, brick: 0, pine: 0, keystone: 0 };
+    var totalDiscounts = { slate: 0, marble: 0, keystone: 0 }; // Kokonaisalennukset
+    var totalStatsList = []; // Kerää statsit listaan
+    var costSummaryElement = document.getElementById('costSummary'); 
+    var numberFormatter = new Intl.NumberFormat('en-US');
+
+    costSummaryElement.innerHTML = '';
+
+    var buildingBlocks = document.querySelectorAll('.buildingBlock');
+    for (let i = 0; i < buildingBlocks.length; i++) {
+        let block = buildingBlocks[i];
+        let enhancementSelect = block.querySelector('.enhancementSelect');
+        let currentLevelInput = block.querySelector('.currentLevel input');
+        let targetLevelInput = block.querySelector('.targetLevel input');
+        let currentLevel = parseInt(currentLevelInput.value);
+        let targetLevel = parseInt(targetLevelInput.value);
+        let selectedPrice = price[enhancementSelect.value];
+        var blockCosts = { slate: 0, marble: 0, brick: 0, pine: 0, keystone: 0 };
+        var blockCostsBeforeDiscount = { ...blockCosts };
+
+        for (let j = currentLevel; j < targetLevel; j++) {
+            blockCosts.slate += selectedPrice[j].Slate;
+            blockCosts.marble += selectedPrice[j].Marble;
+            blockCosts.brick += selectedPrice[j].Brick;
+            blockCosts.pine += selectedPrice[j].Pine;
+            blockCosts.keystone += selectedPrice[j].Keystone;
+        }
+
+        // Laske alennukset ja päivitä kokonaisalennukset
+        var blockDiscounts = { slate: 0, marble: 0, keystone: 0 };
+        for (let key in blockCosts) {
+            blockCostsBeforeDiscount[key] = blockCosts[key];
+            if (key === 'marble') {
+                blockDiscounts.marble = blockCosts[key] * aleMarble;
+                blockCosts[key] -= blockDiscounts.marble;
+            }
+            if (key === 'keystone') {
+                blockDiscounts.keystone = blockCosts[key] * aleKeystone;
+                blockCosts[key] -= blockDiscounts.keystone;
+            }
+            totalCosts[key] += blockCosts[key];
+            totalDiscounts[key] += blockDiscounts[key];
+        }
+
+        var stats = selectedPrice[targetLevel - 1].Value - selectedPrice[currentLevel - 1].Value;
+        var enhancementText = enhancementSelect.options[enhancementSelect.selectedIndex].text;
+        var buildingText = block.querySelector('.buildingSelect').options[block.querySelector('.buildingSelect').selectedIndex].text;
+
+        // Tallenna statsit listalle
+        totalStatsList.push(enhancementText + " +" + stats.toFixed(2) + "%");
+
+        var blockCostDiv = document.createElement('div');
+        blockCostDiv.classList.add('costBox');
+        blockCostDiv.innerHTML = `
+            <h4>${buildingText} - ${enhancementText}</h4>
+            <p class="stats">Stats increase: ${stats.toFixed(2)}%</p>
+        `;
+
+        // Lisää kustannukset ja alennukset costBoxiin
+        for (let key in blockCosts) {
+            blockCostDiv.innerHTML += `
+                <p>${key.charAt(0).toUpperCase() + key.slice(1)}: ${numberFormatter.format(blockCosts[key])}</p>
+                ${blockDiscounts[key] > 0 ? `<span>Cost Efficiency saved on ${key.charAt(0).toUpperCase() + key.slice(1)}: ${numberFormatter.format(blockDiscounts[key])}</span>` : ''}
+            `;
+        }
+        costSummaryElement.appendChild(blockCostDiv);
+    }
+
+    // Luo ja lisää kokonaiskustannusten ja kokonaistatistiikan osio
+    if (buildingBlocks.length > 1) {
+        var totalCostDiv = document.createElement('div');
+        totalCostDiv.classList.add('totalCosts');
+        var statsListHtml = totalStatsList.map(stat => `<p class="stats">${stat}</p>`).join('');
+        totalCostDiv.innerHTML = `
+            <h3>Costs in total:</h3>
+            ${statsListHtml}
+        `;
+
+        // Lisää kokonaiskustannukset ja kokonaisalennukset
+        for (let key in totalCosts) {
+            totalCostDiv.innerHTML += `
+                <p>${key.charAt(0).toUpperCase() + key.slice(1)}: ${numberFormatter.format(totalCosts[key])}</p>
+                ${totalDiscounts[key] > 0 ? `<span>Cost Efficiency saved on ${key.charAt(0).toUpperCase() + key.slice(1)}: ${numberFormatter.format(totalDiscounts[key])}</span>` : ''}
+            `;
+        }
+        costSummaryElement.appendChild(totalCostDiv);
+    }
+}
+
+
+
+
+
+// Kutsu funktioita sivun latautuessa
+document.addEventListener('DOMContentLoaded', function() {
+    var buildingSelectElements = document.querySelectorAll('.buildingSelect');
+    buildingSelectElements.forEach(function(buildingSelectElement) {
+        loadBuildings(buildingSelectElement);
+    });
+
+
+
+	document.querySelectorAll('.buildingSelect').forEach(function(buildingSelectElement) {
+		buildingSelectElement.addEventListener('change', function() {
+			var enhancementSelect = this.parentNode.parentNode.querySelector('.enhancementSelect');
+			loadEnhancements(enhancementSelect);
+		});
+	});
+});
