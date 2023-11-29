9ca83888b1426b9dd73e5b48413c1c6a3c171b36
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2023-11-29 18:30:02 +0200

    Update build.js

--- build.js
+++ build.js
@@ -4,74 +4,74 @@ let build = {
         "Infantry Defense vs. Player's Ranged": "PriceLow",
         "Infantry Defense vs. Player's Cavalry": "PriceLow"
     },
+    "Dragon pit": {
+        "Dragon: Troop Attack at a Seat of Power": "PriceMedium2",
+        "Dragon: Troop Defence at a Seat of Power": "PriceMedium2",
+        "Dragon: Troop Health at a Seat of Power": "PriceMedium2"
+    },
+    "Great hall": {
+        "Infantry Attack vs. Player's Infantry": "PriceLow",
+        "Ranged Attack vs. Player's Ranged": "PriceLow",
+        "Cavalry Attack vs. Player's Cavalry": "PriceLow"
+    },
+    "Keep": {
+        "Troop Attack": "PriceHigh",
+        "Troop Defence": "PriceHigh",
+        "Troop Health": "PriceHigh"
+    },
+    "Master's tower": {
+        "Troop Attack vs. Player": "PriceHigh",
+        "Wall Damage Bonus": "PriceMedium",
+        "March Speed vs. Recources": "PriceLow"
+    },
     "Range": {
         "Ranged Attack vs. Player": "PriceMedium",
         "Ranged Defense vs. Player's Infantry": "PriceLow",
         "Ranged Defense vs. Player's Cavalry": "PriceLow"
+    },
+    "Rookery": {
+        "Infantry Attack vs. Player's Ranged": "PriceLow",
+        "Ranged Attack vs. Player's Cavalry": "PriceLow",
+        "Cavalry Attack vs. Player's Infantry": "PriceLow"
+    },
+    "Shrine": {
+        "Troop Health vs. Player": "PriceHigh",
+        "March Speed vs. Seat of Power": "PriceHigh",
+        "Troop Defence vs. Player": "PriceMedium"
+    },
+    "Smith": {
+        "Infantry Attack vs. Player's Cavalry": "PriceLow",
+        "Ranged Attack vs. Player's Infantry": "PriceLow",
+        "Cavalry Attack vs. Player's Ranged": "PriceLow"
     },
     "Stable": {
         "Cavalry Attack vs. Player": "PriceMedium",
         "Cavalry Defense vs. Player's Infantry": "PriceLow",
         "Cavalry Defense vs. Player's Ranged": "PriceLow"
     },
-	"Workshop": {
-        "Siege Attack vs Player's Troops": "PriceMedium",
-        "Siege Defense vs Player's Troops": "PriceMedium",
-        "Siege Health vs Player's Troops": "PriceMedium"
-    },
-	"Wall": {
-        "Infantry Defense vs. Player": "PriceMedium",
-        "Ranged Defense vs. Player": "PriceMedium",
-        "Cavalry Defense vs. Player": "PriceMedium"
-    },
-	"Shrine": {
-        "Troop Health vs. Player": "PriceHigh",
-        "March Speed vs. Seat of Power": "PriceHigh",
-        "Troop Defence vs. Player": "PriceMedium"
-    },
-	"Training yard": {
-        "Infantry Defense vs. Player's Infantry": "PriceLow",
-        "Ranged Defense vs. Player's Ranged": "PriceLow",
-        "Cavalry Defense vs. Player's Cavalry": "PriceLow"
-    },
-	"Great hall": {
-        "Infantry Attack vs. Player's Infantry": "PriceLow",
-        "Ranged Attack vs. Player's Ranged": "PriceLow",
-        "Cavalry Attack vs. Player's Cavalry": "PriceLow"
-    },
-	"Storehouse": {
+    "Storehouse": {
         "Infantry Health vs. Player": "PriceMedium",
         "Ranged Health vs. Player": "PriceMedium",
         "Cavalry Health vs. Player": "PriceMedium"
     },
-	"Smith": {
-        "Infantry Attack vs. Player's Cavalry": "PriceLow",
-        "Ranged Attack vs. Player's Infantry": "PriceLow",
-        "Cavalry Attack vs. Player's Ranged": "PriceLow"
+    "Training yard": {
+        "Infantry Defense vs. Player's Infantry": "PriceLow",
+        "Ranged Defense vs. Player's Ranged": "PriceLow",
+        "Cavalry Defense vs. Player's Cavalry": "PriceLow"
     },
-	"Rookery": {
-        "Infantry Attack vs. Player's Ranged": "PriceLow",
-        "Ranged Attack vs. Player's Cavalry": "PriceLow",
-        "Cavalry Attack vs. Player's Infantry": "PriceLow"
+    "Wall": {
+        "Infantry Defense vs. Player": "PriceMedium",
+        "Ranged Defense vs. Player": "PriceMedium",
+        "Cavalry Defense vs. Player": "PriceMedium"
     },
-	"Master's tower": {
-        "Troop Attack vs. Player": "PriceHigh",
-        "Wall Damage Bonus": "PriceMedium",
-        "March Speed vs. Recources": "PriceLow"
-    },
-	"Watchtower": {
+    "Watchtower": {
         "March Speed": "PriceMedium",
         "March Speed Reinforcing": "PriceLow",
         "March Speed vs. Creatures": "PriceLow"
     },
-	"Keep": {
-        "Troop Attack": "PriceHigh",
-        "Troop Defence": "PriceHigh",
-        "Troop Health": "PriceHigh"
-    },
-	"Dragon pit": {
-        "Dragon: Troop Attack at a Seat of Power": "PriceMedium2",
-        "Dragon: Troop Defence at a Seat of Power": "PriceMedium2",
-        "Dragon: Troop Health at a Seat of Power": "PriceMedium2"
+    "Workshop": {
+        "Siege Attack vs Player's Troops": "PriceMedium",
+        "Siege Defense vs Player's Troops": "PriceMedium",
+        "Siege Health vs Player's Troops": "PriceMedium"
     }
 }
