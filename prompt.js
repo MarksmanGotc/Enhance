799e7a5ed049f65be33e2e392626d9e0410e901f
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2023-12-12 13:30:29 +0200

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -2,39 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     var consent = localStorage.getItem("cookieConsent");
     var cookieBanner = document.getElementById("cookieConsentBanner");
 
-    // Jos käyttäjä ei ole vielä tehnyt valintaa, näytä evästebanneri
     if (consent === null) {
         cookieBanner.style.display = "block";
     } else {
         cookieBanner.style.display = "none";
-        // Asetetaan GA4:n tila käyttäjän valinnan mukaan
-        window['ga-disable-G-0LX7SEF348'] = consent !== "accepted";
+        if (consent === "accepted") {
+            gtag('config', 'G-0LX7SEF348', { 'anonymize_ip': false });
+        }
     }
 
     // Käsittele hyväksymisnappi
     document.getElementById("acceptCookies").addEventListener("click", function() {
         localStorage.setItem("cookieConsent", "accepted");
         cookieBanner.style.display = "none";
-        window['ga-disable-G-0LX7SEF348'] = false;
+        gtag('config', 'G-0LX7SEF348', { 'anonymize_ip': false });
     });
 
     // Käsittele kieltäytymisnappi
     document.getElementById("declineCookies").addEventListener("click", function() {
         localStorage.setItem("cookieConsent", "declined");
         cookieBanner.style.display = "none";
-        window['ga-disable-G-0LX7SEF348'] = true;
     });
-	
-	document.getElementById("changeCookieSettings").addEventListener("click", function() {
-		// Poista aiempi suostumus localStoragesta
-		localStorage.removeItem("cookieConsent");
-
-		// Näytä evästebanneri uudelleen
-		document.getElementById("cookieConsentBanner").style.display = "block";
-		gtag('event', 'cookie_settings_click', {
-			'event_category': 'Cookie Actions',
-			'event_label': 'Open cookie prompt'
-		});
-
-	});
 });
