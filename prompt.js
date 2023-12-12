4ae969ed04a7bfc3a3a420d66513482382d700da
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2023-12-12 13:43:10 +0200

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var consent = localStorage.getItem("cookieConsent");
     var cookieBanner = document.getElementById("cookieConsentBanner");
 
+    // Tarkista ja käsittele aiempi suostumus
     if (consent === null) {
         cookieBanner.style.display = "block";
     } else {
         cookieBanner.style.display = "none";
         if (consent === "accepted") {
+            // Poista IP-anonymisointi ja käynnistä normaali GA-seuranta
             gtag('config', 'G-0LX7SEF348', { 'anonymize_ip': false });
         }
     }
@@ -22,5 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("declineCookies").addEventListener("click", function() {
         localStorage.setItem("cookieConsent", "declined");
         cookieBanner.style.display = "none";
+        // IP-anonymisointi pysyy aktiivisena
+    });
+
+    // Käsittele asetusten muuttamisnappi
+    document.getElementById("changeCookieSettings").addEventListener("click", function() {
+        // Poista aiempi suostumus localStoragesta
+        localStorage.removeItem("cookieConsent");
+
+        // Näytä evästebanneri uudelleen
+        cookieBanner.style.display = "block";
+        // Vaihda GA-seuranta takaisin anonyymimuotoon
+        gtag('config', 'G-0LX7SEF348', { 'anonymize_ip': true });
     });
 });
